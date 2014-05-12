47cee0c8b000d7867f2f3250906839988a803e71
Robinde67 <robinde67@gmail.com>
2014-05-12 11:27:31 +0200

    Fixed some textures (again?)

---
+++ Assets/Scipts/HUD.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour
+{
+	public Main m_xMain;
+
+	public GUIStyle m_xGUIStyle;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
